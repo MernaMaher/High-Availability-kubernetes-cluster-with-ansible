9c6668899eecb4a3afe7ab4a1d55c367b72ac43b
osboxes.org <osboxes@loadbalancer1.us-west4-b.c.pelagic-pathway-355011.internal>
2022-07-20 04:16:22 -0400

    Added my project

---
+++ mastertoken.sh
@@ -0,0 +1,5 @@
+# BEGIN ANSIBLE MANAGED BLOCK
+  kubeadm join 10.182.0.2:6443 --token poooii.48yo6o4h9p0z31m5 \
+	--discovery-token-ca-cert-hash sha256:37e6e0fd40d0734b0d994097b14154b3f4e8180d8b8627a4b6476c50fe9d21bb \
+	--control-plane --certificate-key 893e365739f877bab35749e5e0ada9017a269427f61f77ef78c21059a84fc756
+# END ANSIBLE MANAGED BLOCK
---
+++ workertoken.sh
@@ -0,0 +1,4 @@
+# BEGIN ANSIBLE MANAGED BLOCK
+kubeadm join 10.182.0.2:6443 --token poooii.48yo6o4h9p0z31m5 \
+	--discovery-token-ca-cert-hash sha256:37e6e0fd40d0734b0d994097b14154b3f4e8180d8b8627a4b6476c50fe9d21bb 
+# END ANSIBLE MANAGED BLOCK
